936b1d2b278329e15d7c28535380a48ec2aca76a
prydt <pranoydll@gmail.com>
2017-05-31 18:48:31 -0500

    improved linked-list support

--- data-structures/linked-list/LinkedList.h
+++ data-structures/linked-list/LinkedList.h
@@ -10,135 +10,112 @@
  *
  */
 template <class T>
-class LinkedList
-{
-  public:
-    // constructor
-    LinkedList()
-    {
-      Node* ptr = new Node();
-      ptr->next = nullptr;
-      head = ptr;
-      count = 1;
-    }
+class LinkedList {
+ public:
+  // constructor
+  LinkedList() {
+    head = nullptr;
+    count = 0;
+  }
 
-    // destructor
-    ~LinkedList()
-    {
-      while(length() != 0)
-        removeHead();
-    }
+  // destructor
+  ~LinkedList() {
+    while (count != 0) removeHead();
+  }
 
-    // returns length of list
-    int length()
-    {
-      return count;
-    }
+  // returns length of list
+  int length() { return count; }
 
-    // adds node to head
-    void push(T val)
-    {
-      Node* newNode = new Node();
-      newNode->data = val;
-      newNode->next = head;
+  // adds node to head
+  void push(T val) {
+    Node* newNode = new Node();
+    newNode->data = val;
+    newNode->next = head;
+    head = newNode;
+    count++;
+  }
+
+  // adds nodes to tail
+  void append(T val) {
+    Node* current = head;
+    Node* newNode = new Node();
+
+    newNode->data = val;
+    newNode->next = nullptr;
+
+    if (current == nullptr)
       head = newNode;
-      count++;
+    else {
+      while (current->next != nullptr) current = current->next;
+
+      current->next = newNode;
+    }
+    count++;
+  }
+
+  // removes from head
+  T removeHead() {
+    Node* tmp = head;
+    T out = head->data;
+    head = head->next;  // simply sets head to the next node in the list
+
+    delete tmp;
+
+    count--;
+    return out;
+  }
+
+  // removes last item
+  T removeTail() {
+    Node* tmp = head;
+    int out;
+
+    while (tmp->next->next != nullptr) tmp = tmp->next;
+
+    out = tmp->next->data;
+    delete tmp->next;
+    tmp->next = nullptr;
+
+    count--;
+    return out;
+  }
+
+  // prints out list
+  void print() {
+    Node* tmp = head;
+    std::cout << "[ ";
+    while (tmp != nullptr) {
+      std::cout << tmp->data << " -> ";
+      tmp = tmp->next;
+    }
+    std::cout << "nullptr ]\n";
+  }
+
+  // gets items in at the specified index
+  T& operator[](std::size_t index) {
+    Node* tmp = head;  // temp variable to store the head pointer
+    int i = 0;         // iterator
+
+    while (tmp->next != nullptr && i < (int)index) {
+      tmp = tmp->next;
+      i++;
     }
 
-    // adds nodes to tail
-    void append(T val)
-    {
-      Node* current = head;
-      Node* newNode = new Node();
+    return tmp->data;
+  }
 
-      newNode->data = val;
-      newNode->next = nullptr;
+ private:
+  // building block of the list...
+  // the list is just a bunch of nodes
+  struct Node {
+    T data;
+    Node* next;
+  };
 
-      if(current == nullptr)
-        head = newNode;
-      else
-      {
-        while(current->next != nullptr)
-          current = current->next;
+  // head pointer of the list
+  Node* head;
 
-        current->next = newNode;
-      }
-      count++;
-    }
-
-    // removes from head
-    T removeHead()
-    {
-      Node* tmp = head;
-      int out = head->data;
-      head = head->next; // simply sets head to the next node in the list
-
-      delete tmp;
-
-      count--;
-      return out;
-    }
-
-    // removes last item
-    T removeTail()
-    {
-      Node* tmp = head;
-      int out;
-
-      while(tmp->next->next != nullptr)
-        tmp = tmp->next;
-
-      out = tmp->next->data;
-      delete tmp->next;
-      tmp->next = nullptr;
-
-      count--;
-      return out;
-    }
-
-    // prints out list
-    void print()
-    {
-      Node* tmp = head;
-      std::cout << "[ ";
-      while(tmp->next != nullptr)
-      {
-        std::cout << tmp->data << ' ';
-        tmp = tmp->next;
-      }
-      std::cout << "]\n";
-    }
-
-    // gets items in at the specified index
-    T& operator[](std::size_t index)
-    {
-      Node* tmp = head;   // temp variable to store the head pointer
-      int i = 0;          // iterator
-
-      while( tmp->next != nullptr && i < (int) index )
-      {
-        tmp = tmp->next;
-        i++;
-      }
-
-      return tmp->data;
-
-    }
-
-
-  private:
-    // building block of the list...
-    // the list is just a bunch of nodes
-    struct Node
-    {
-      T data;
-      Node* next;
-    };
-
-    // head pointer of the list
-    Node* head;
-
-    // length
-    int count;
+  // length
+  int count;
 };
 #endif
--- data-structures/linked-list/test.cpp
+++ data-structures/linked-list/test.cpp
@@ -1,23 +1,20 @@
 
-#include "LinkedList.h"
 #include <iostream>
+#include "LinkedList.h"
 
 /*
  * Output of this program:
  *
- * Length: 20 List: [ 0 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 ]
- * Length: 17 List: [ 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 ]
- * Get[2]: 4
+ *  Length: 20 List: [ 0 -> 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> 8 -> 9 -> 10 -> 11 -> 12 -> 13 -> 14 -> 15 -> 16 -> 17 -> 18 -> 19 -> nullptr ]
+ *  Length: 17 List: [ 3 -> 4 -> 5 -> 6 -> 7 -> 8 -> 9 -> 10 -> 11 -> 12 -> 13 -> 14 -> 15 -> 16 -> 17 -> 18 -> 19 -> nullptr ]
+ *  Get[2]: 5
  *
  */
 
-
-int main()
-{
+int main() {
   LinkedList<int> list;
 
-  for(int i = 0; i < 20; i++)
-    list.append(i);
+  for (int i = 0; i < 20; i++) list.append(i);
 
   std::cout << "Length: " << list.length() << " List: ";
   list.print();
